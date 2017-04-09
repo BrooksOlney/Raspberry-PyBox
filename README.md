3e838063fd8e8cedcfde1cd67c056a04b2052585
Chris <cgado12@gmail.com>
2017-04-08 21:29:52 -0400

    updated readme, added comments, organized code

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # Raspberry-PyBox
-Regularly backup files from Dropbox using the Python SDK
+## Regularly backup files from Dropbox using the Python SDK
+
+
+ ###### if you want to do more with accessing backup files create your own dropbox app and change the TOKEN variable in main
+
+
+###### questions/concerns/notes in main.py:
+   - line 7
+   - can main be moved to the top?
+   - c++ uses a set precision function to make text more
+      accurate, maybe python has one so we can make the log file like a table
+   - put all functions/code regarding token/backuPath/dbx init into another folder, then in main just call the function to access the needed instruction/information
+   - create delete and ignore function to delete or ignore certain files
+       - ignore will disregard given file/folder while preforming backup/restore process
+   - separate update function to backup or restore based on choice
+
+
+
+ program cycle:
+   - calls main -> line 102
+   - gets files and folders -> line 43
+   - comparing directories -> line 65
+   - update directories -> line 93
--- main.py
+++ main.py
@@ -19,23 +19,36 @@ import os
 from os import listdir
 from os.path import isfile, join
 
+
+# **********************************
 # authorization token, generated from development page of Dropbox
-TOKEN = 'S-aVck8tNgAAAAAAAAAAIInlY4_XleRSGvTFV95_fglkcLh51JOHGg66tq_bV-3z'
+TOKEN = 'C27L8RWZZpkAAAAAAAANfcaGjLoOvAKsJ_7e3DYHc-zYifjRDDaO-F7JIm6_TEWM'
+
+# this is brooks token, its the one we used in the hackathon
+# TOKEN = 'S-aVck8tNgAAAAAAAAAAIInlY4_XleRSGvTFV95_fglkcLh51JOHGg66tq_bV-3z'
+# **********************************
+
+
+
+# ********** REMEMBER TO CHANGE BACKUPPATH FILE PATH TO YOUR MACHINES FILEPATH*******
 
 # backup path and log file that will be created/modified inside of it
-# ********** REMEMBER TO CHANGE BACKUPPATH FILE PATH TO YOUR MACHINES FILEPATH*******
-BACKUPPATH = '~/github/Raspberry-PyBox/backupPrac/'
+BACKUPPATH = 'backupPrac/'
 if(os.path.exists(BACKUPPATH + 'log.txt')):
     LOG = open(BACKUPPATH + 'log.txt', 'w')
 else:
     LOG = open(BACKUPPATH + 'log.txt', 'aw')
+LOG.write('\t  Files \t\t\t\t  Date : Time\n')
+
 
 #get file names in dropbox root
 def get_files_and_folders(dbx):
-    allFiles = dict()
+    allFiles = dict()   # stores metadata in key value pair structure
     otherDirectories = []
     client = dropbox.client.DropboxClient(TOKEN)
     metadata = client.metadata('/')
+
+    print("\t - Gathering file paths")
     files = [content['path'].split('/')[-1] for content in metadata['contents']]
     for x in files:
         if x.find('.') > -1:
@@ -44,24 +57,30 @@ def get_files_and_folders(dbx):
         else:
             otherDirectories.append(x)
 
-
     compare_to_local(dbx, allFiles)
 
+
 # compare dropbox files to files in backup location, note differences
+# remote to local
+# files contains the meta data for each file
 def compare_to_local(dbx, files):
+    # can localFiles be an array/list?
     localFiles = dict()
     downloadThese = []
     uploadThese = []
 
+    #This is comparing the time since last modification of local to remote
     temp = [f for f in listdir(BACKUPPATH) if isfile(join(BACKUPPATH, f))]
     for path in temp:
         file_mod_time = os.stat(BACKUPPATH + path).st_mtime
         localFiles[path] = file_mod_time;
 
-    for x, y in files.items():
+    print("\t - Comparing directories")
+
+    for x, y in files.items():        # what is the metadata it is searching through
         downloadThese.append(x)
 
-    for x, y in localFiles.items():
+    for x, y in localFiles.items():   # iterating through each files mod time
         inDropbox = 0
         for w, z in files.items():
             if(w == x):
@@ -69,41 +88,72 @@ def compare_to_local(dbx, files):
         if(inDropbox == 0):
             uploadThese.append(x)
 
-
     updateDirectories(dbx, downloadThese, uploadThese)
+
 
 # upload files from local backup, download files from dropbox
 def updateDirectories(dbx, downloadThese, uploadThese):
+    print("\t - Updating your directories")
     dropBoxPath = ""
 
     for path in downloadThese:
         dbx.files_download_to_file(BACKUPPATH + path, '/' + path)
-        LOG.write(path + ' has been downloaded to ' + BACKUPPATH +  ' at ' + datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S') + '\n\n')
+        LOG.write(path + ' \t\t\t '
+                + datetime.datetime.now().strftime('%Y-%m-%d : %H:%M:%S') + '\n\n')
 
     for path in uploadThese:
         if(path != "log.txt"):
             dbx.files_upload(BACKUPPATH + path, '/' + path, mode=WriteMode('add'))
-        LOG.write(path + ' has been uploaded to Dropbox/ at ' + datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S') + '\n\n')
+            # writing to log!
+            LOG.write(path + ' \t\t\t '
+                    + datetime.datetime.now().strftime('%Y-%m-%d : %H:%M:%S') + '\n\n')
 
 
+
+    print("\t - Syncing has finished\n")
+    print("Consult log.txt for further information\n")
+
+
+# **************************** This is where the program starts *****************
 if __name__ == '__main__':
+
     # Check for an access token
     if (len(TOKEN) == 0):
         sys.exit("ERROR: Looks like you didn't add your access token. "
             "Open up backup-and-restore-example.py in a text editor and "
             "paste in your token in line 14.")
 
+
     # Create an instance of a Dropbox class, which can make requests to the API.
     print("Initiating connection with Dropbox...")
     dbx = dropbox.Dropbox(TOKEN)
-
-    get_files_and_folders(dbx)
-
     # Check that the access token is valid
     try:
         dbx.users_get_current_account()
     except AuthError as err:
         sys.exit("ERROR: Invalid access token; try re-generating an "
             "access token from the app console on the web.")
-    print("Syncing has finished, consult the log for details.")
+
+    '''
+    #works but functions are not created
+    # consider selection menu
+
+    print('what would you like to do?')
+    print('\t 1) Backup 2) Restore \n\t 2) Delete \n\t 3) Ignore\n\n')
+    usrChoice = raw_input('Please enter 1, 2, or 3: ')
+
+    if (usrChoice == 1):
+        get_files_and_folders(dbx)
+    elif (usrChoice == 2)):
+        call restore function
+    ilif (usrChoice == 3):
+        Call delete function:
+    else (usrChoice == 4:
+        Call ignore function)
+    '''
+
+    # **** Start program cycle ****
+    get_files_and_folders(dbx)
+
+
     LOG.close()
